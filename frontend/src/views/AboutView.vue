413904c7dc804289fb626df9763f165d1c627e88
Erica Kresch <ekresch15@gmail.com>
2022-12-23 20:43:31 -0600

    next steps

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,3 +1,9 @@
+<!-- NEXT STEPS: Place ships
+  Will need to store ships & sizes somewhere
+FLOW: go one ship at a time. Click starting cell, highlight endpoints. Click endpoint
+Hit confirm button before moving to next ship
+Once all ships placed use submit_ships feature -->
+
 <script>
 import GameBoard from "@/components/GameBoard.vue";
 import { useGameStore } from "@/stores/GameStore";
@@ -22,6 +28,7 @@ export default {
     </div>
 
     <GameBoard />
+    <button @click="this.gameStore.submit_ships()">Submit Ships</button>
   </div>
 </template>
 
